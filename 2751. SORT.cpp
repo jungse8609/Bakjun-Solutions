702928d555060462ee1e3fecf1fa87806ae5fe30
jungse8609 <69056797+jungse8609@users.noreply.github.com>
2022-05-26 13:19:50 +0000

    문제 파악. 그냥 내가 문제 요구를 충족 못 시켜버림

--- 2751. SORT.cpp
+++ 2751. SORT.cpp
@@ -5,70 +5,60 @@
 
 아래 코드는 병합 정렬(merge sort)에 대한 것임
 힙 정렬(heap sort) 구현에 대해서도 공부 필요
+========================================
+걍 sort -> stable_sort 하면 해결됨...
+sort : 불안정 정렬 - 비교 순위 동일 시 어찌 정렬될지 모름
+stable_sort : 안정 정렬 - 비교 순위 동일 시 index 순서로 정렬(그대로 유지)
 */
 
 #include <iostream>
+#include <string>
 #include <vector>
+#include <numeric>
 #include <algorithm>
-
 using namespace std;
 
-template<typename T>
-void printVector(const vector<T> &vec) {
-    for (auto &i : vec) {
-        cout << i << '\n';
+class Age {
+public:
+    int age;
+    string name;
+    int id;
+
+    Age(int age, string name, int id) {
+        this->age = age;
+        this->name = name;
+        this->id = id;
     }
-    cout << '\n';
+};
+
+bool compare_age(Age a, Age b) {
+    if (a.age == b.age) {
+        return a.id < b.id;
+    }
+    else {
+        return a.age < b.age;
+    }
 }
 
-template<typename T>
-void merge(vector<T> &vec, vector<T> &v1, vector<T> &v2) {
-    auto siz1 = v1.size();
-    auto siz2 = v2.size();
-    size_t p1 = 0;
-    size_t p2 = 0;
+int main(void)
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-    while (p1 < siz1 && p2 < siz2) {
-        if (v1.at(p1) < v2.at(p2))
-            vec.push_back(v1.at(p1++));
-        else
-            vec.push_back(v2.at(p2++));
+    int x;
+    cin >> x;
+    vector<Age> v;
+    for (int i = 0; i < x; i++) {
+        int a;
+        string b;
+        cin >> a >> b;
+        Age age = Age(a, b, i);
+        v.push_back(age);
     }
 
-    while (p1 < siz1) vec.push_back(v1.at(p1++));
-    while (p2 < siz2) vec.push_back(v2.at(p2++));
-}
-
-template<typename T>
-void mergeSort(vector<T> &vec) {
-    if (vec.size() <= 1)
-        return;
-
-    auto iter = vec.begin() + vec.size() / 2;
-    vector<T> v1 (vec.begin(), iter);
-    vector<T> v2 (iter, vec.end());
-
-    mergeSort(v1); // 정렬된 v1과
-    mergeSort(v2); // 정렬된 v2가 완성!!
-                    // 그걸 아래서 합치면 끝!!
-    vec.clear();    
+    stable_sort(v.begin(), v.end(), compare_age);
     
-    merge(vec, v1, v2);
-}
-
-int main() {
-    int N;
-    cin >> N;
-
-    vector<int> vec1;
-
-    for (int i=0; i<N; i++) {
-        int num;
-        cin >> num;
-        vec1.push_back(num);
+    for (int i = 0; i < x; i++) {
+        cout << v[i].age << " " << v[i].name << "\n";
     }
-
-    // printVector(vec1);
-    mergeSort(vec1);
-    printVector(vec1);
+    return 0;
 }
